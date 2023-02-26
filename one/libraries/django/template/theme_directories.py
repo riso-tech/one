21daa5190d39b8e1730e42170c0cdb4e72a2174d
Bin Nguyễn <bin.ngoctu@gmail.com>
2023-02-26 15:41:24 +0700

    Fix test

--- one/libraries/django/template/theme_directories.py
+++ one/libraries/django/template/theme_directories.py
@@ -5,7 +5,13 @@ from django.template.loaders.filesystem import Loader as BaseLoader
 class Loader(BaseLoader):
     def get_dirs(self):
         dirs = super().get_dirs()
-        return [
-            self.engine.dirs[0] + "/ui/" + config.UI_THEME_SELECT,
-            self.engine.dirs[0] + "/admin/" + config.ADMIN_THEME_SELECT,
-        ] + dirs
+        try:
+            return [
+                self.engine.dirs[0] + "/ui/" + config.UI_THEME_SELECT,
+                self.engine.dirs[0] + "/admin/" + config.ADMIN_THEME_SELECT,
+            ] + dirs
+        except Exception:  # noqa
+            return [
+                self.engine.dirs[0] + "/ui/default",
+                self.engine.dirs[0] + "/admin/adminlte",
+            ] + dirs
